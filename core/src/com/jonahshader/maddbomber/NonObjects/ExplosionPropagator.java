b74a9b1eee105a27ac47a8d5ccaa06c4e4175a4d
Jonah Shader <jonah.shader@gmail.com>
2018-03-09 14:35:39 -0500

    Lots of stuff

--- core/src/com/jonahshader/maddbomber/NonObjects/ExplosionPropagator.java
+++ core/src/com/jonahshader/maddbomber/NonObjects/ExplosionPropagator.java
@@ -1,6 +1,9 @@
 package com.jonahshader.maddbomber.NonObjects;
 
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.jonahshader.maddbomber.GameWorld;
+
+import java.util.ArrayList;
 
 public class ExplosionPropagator {
     public enum Direction {
@@ -11,13 +14,49 @@ public class ExplosionPropagator {
         ALL
     }
 
-    int tileX, tileY, explosionRadius;
-    Direction direction;
-    GameWorld world;
+    public class ExplosionSpreader{
 
-    public ExplosionPropagator(int tileX, int tileY, int explosionRadius, Direction direction, GameWorld gameWorld) {
-//asdf
+        ExplosionSpreader(int tileX, int tileY, int explosionsRemaining, Direction direction, GameWorld gameWorld, ArrayList<ExplosionSpreader> spreaders) {
+            TiledMapTileLayer explodables = (TiledMapTileLayer) gameWorld.getMap().getLayers().get("Explodable");
+            TiledMapTileLayer walls = (TiledMapTileLayer) gameWorld.getMap().getLayers().get("Walls");
+
+            boolean hitExplodable = explodables.getCell(tileX, tileY) != null;
+
+            //create additional explosions
+            if (!hitExplodable) {
+                if (explosionsRemaining > 0) {
+                    switch (direction) {
+                        case LEFT:
+                            if (walls.getCell(tileX - 1, tileY) == null) {
+                                spreaders.add(new ExplosionSpreader(tileX - 1, tileY, explosionsRemaining - 1, direction, gameWorld, spreaders));
+                            }
+                            break;
+                        case RIGHT:
+                            if (walls.getCell(tileX + 1, tileY) == null) {
+                                spreaders.add(new ExplosionSpreader(tileX + 1, tileY, explosionsRemaining - 1, direction, gameWorld, spreaders));
+                            }
+                            break;
+                        case UP:
+                            if (walls.getCell(tileX, tileY + 1) == null)
+                                spreaders.add(new ExplosionSpreader(tileX, tileY + 1, explosionsRemaining - 1, direction, gameWorld, spreaders));
+                            break;
+                        case DOWN:
+                            if (walls.getCell(tileX, tileY - 1) == null)
+                                spreaders.add(new ExplosionSpreader(tileX, tileY - 1, explosionsRemaining - 1, direction, gameWorld, spreaders));
+                            break;
+                        case ALL:
+                            if (walls.getCell(tileX - 1, tileY) == null)
+                                spreaders.add(new ExplosionSpreader(tileX - 1, tileY, explosionsRemaining - 1, Direction.LEFT, gameWorld, spreaders));
+                            if (walls.getCell(tileX + 1, tileY) == null)
+                                spreaders.add(new ExplosionSpreader(tileX + 1, tileY, explosionsRemaining - 1, Direction.RIGHT, gameWorld, spreaders));
+                            if (walls.getCell(tileX, tileY + 1) == null)
+                                spreaders.add(new ExplosionSpreader(tileX, tileY + 1, explosionsRemaining - 1, Direction.UP, gameWorld, spreaders));
+                            if (walls.getCell(tileX, tileY - 1) == null)
+                                spreaders.add(new ExplosionSpreader(tileX, tileY - 1, explosionsRemaining - 1, Direction.DOWN, gameWorld, spreaders));
+                            break;
+                    }
+                }
+            }
+        }
     }
-
-    //TODO: create sub class for actual explosions. these do the actual propogation and returns their data to an array stored in its parent class (this)
 }
